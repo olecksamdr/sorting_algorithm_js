22f4c1d8fba9bb2a82eb5376fe30c0a4b915a414
olecksandr.veleshchuk@digitalevo.io <velsashok@gmail.com>
2017-02-28 22:38:41 +0200

    fix tabs

--- sortingAlgorithm.js
+++ sortingAlgorithm.js
@@ -1,13 +1,13 @@
 function bubleSort(array) {
-	var savedValue;
-  
-	for (var i = 1; i < array.length; ++i) {
-  	for(var j = array.length - 1; j >= i; --j) {
-    		if (array[j] < array[j - 1]) {
-        	savedValue = array[j];
-          array[j] = array[j - 1];
-          array[j - 1] = savedValue;
-        }
+  var savedValue;
+
+  for (var i = 1; i < array.length; ++i) {
+    for(var j = array.length - 1; j >= i; --j) {
+      if (array[j] < array[j - 1]) {
+        savedValue = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = savedValue;
+      }
     }
   }
   
@@ -15,13 +15,13 @@ function bubleSort(array) {
 }
 
 function insertionSort(array) {
-	var j, key
-  
+  var j, key
+
   for (var i = 1; i < array.length; ++i) {
-  	key = array[i];
+    key = array[i];
     j = i - 1;
     while(j >= 0 && array[j] > key) {
-    	array[j + 1] = array[j];
+      array[j + 1] = array[j];
       --j;
     }
     array[j + 1] = key;
@@ -31,13 +31,13 @@ function insertionSort(array) {
 }
 
 function selectionSort(array) { 
-	// used for swap
+  // used for swap
   var storevalue;
   for (var k, i = 0; i < array.length; ++i) {
-  	k = i;
+    k = i;
     for (var j = i + 1; j <= array.length; ++j) {
-    	if (array[j] < array[k])
-      	k = j;
+      if (array[j] < array[k])
+        k = j;
     }
     storeValue = array[i];
     array[i] = array[k];
